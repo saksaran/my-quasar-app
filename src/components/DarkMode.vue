2b7885a2787939d55526b3925e5116faf187bc75
Saksaran <saksaran.akra@kmutt.ac.th>
2025-01-17 11:36:23 +0700

    add table and dark mode

---
+++ src/components/DarkMode.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="q-pa-md">
+    <div>
+      <q-toggle
+        v-model="isDark"
+        label="Dark Mode"
+        color="primary"
+        @update:model-value="toggleDarkMode"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { Dark } from 'quasar'
+
+const isDark = ref(Dark.isActive)
+
+const toggleDarkMode = () => {
+  Dark.set(isDark.value)
+}
+</script>
+
+<style></style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,16 @@
 <template>
-  <q-page class="flex flex-center">
+  <!-- <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
-  </q-page>
+    />
+  </q-page> -->
+  <HelloWorld />
+  <DarkMode />
 </template>
 
 <script setup>
-//
+import HelloWorld from 'components/HelloWorld.vue'
+import DarkMode from 'components/DarkMode.vue'
 </script>
